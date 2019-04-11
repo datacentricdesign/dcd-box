037052eba9d592da7d3a9171973c4e41cff8be78
Jacky Bourgeois - IO <jbourgeois@dwd.tudelft.nl>
2019-04-11 21:21:19 +0200

    config

--- scripts/services/hydra.sh
+++ scripts/services/hydra.sh
@@ -10,12 +10,12 @@ client_path="$path/clients/*.json"
 echo "Deleting clients in $client_path..."
 for filename in $client_path; do
     id=$(getclientid $filename)
-    (set -x; hydra clients delete $id || true)
+    (set -x; hydra clients --fake-tls-termination delete $id || true)
 done
 echo "Deleted all clients in $client_path!"
 
 echo "Importing clients in $client_path..."
 for filename in $client_path; do
-    (set -x; hydra clients import $filename)
+    (set -x; hydra clients --fake-tls-termination import $filename)
 done
 echo "Imported all clients in $client_path!"

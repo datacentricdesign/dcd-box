8bc85f6f0373f88343d72e7356d68a77ff125c4a
Jacky Bourgeois <j.bourgeois@tudelft.nl>
2019-09-03 08:11:08 +0200

    commenting configurator's role

--- scripts/services/keto.sh
+++ scripts/services/keto.sh
@@ -21,19 +21,19 @@ source "$( dirname "${BASH_SOURCE[0]}" )/../helper/getid.sh"
 #done
 #echo "Imported all roles in $role_path!"
 
-url=$1
-path=$2
-policy_path="$path/policies/*.json"
+#url=$1
+#path=$2
+#policy_path="$path/policies/*.json"
 
-echo "Deleting policies in $policy_path..."
-for filename in $policy_path; do
-    id=$(getid $filename)
-    (set -x; keto engines acp ory policies delete --endpoint $url exact $id || true)
-done
-echo "Deleted all policies in $policy_path!"
+#echo "Deleting policies in $policy_path..."
+#for filename in $policy_path; do
+#    id=$(getid $filename)
+#    (set -x; keto engines acp ory policies delete --endpoint $url exact $id || true)
+#done
+#echo "Deleted all policies in $policy_path!"
 
-echo "Importing policies in $policy_path..."
-for filename in $policy_path; do
-    (set -x; keto engines acp ory policies import --endpoint $url exact $filename)
-done
-echo "Imported all policies in $policy_path!"
+#echo "Importing policies in $policy_path..."
+#for filename in $policy_path; do
+#    (set -x; keto engines acp ory policies import --endpoint $url exact $filename)
+#done
+#echo "Imported all policies in $policy_path!"
